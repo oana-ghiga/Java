0f083b988c775c65719bf92744cc76f62f6df40d
Oana Ghiga <oana.r.maria@gmail.com>
2023-02-28 15:55:48 +0200

    added the route class

---
+++ Labs/src/lab2/compulsory/Route.java
@@ -0,0 +1,48 @@
+package lab2.compulsory;
+import java.util.List;
+
+// Route class
+public class Route {
+    private Location start;
+    private Location end;
+    private List<Road> roads;
+    public Route(Location start, Location end, List<Road> roads) {
+        this.start = start;
+        this.end = end;
+        this.roads = roads;
+    }
+
+
+    public Location getStart() {
+        return start;
+    }
+
+    public void setStart(Location start) {
+        this.start = start;
+    }
+
+    public Location getEnd() {
+        return end;
+    }
+
+    public void setEnd(Location end) {
+        this.end = end;
+    }
+
+    public List<Road> getRoads() {
+        return roads;
+    }
+
+    public void setRoads(List<Road> roads) {
+        this.roads = roads;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "start=" + start +
+                ", end=" + end +
+                ", roads=" + roads +
+                '}';
+    }
+}
